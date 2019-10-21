dd866a7bf59b3d47960a4dbb9b2db32b28d38bdc
moriryota1218 <mryota5618@gmail.com>
2019-10-21 22:18:49 +0900

    propsで値を渡す

--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@
 // Components(部品の再利用)
 
 var likeComponent = Vue.extend({
+  // カスタム属性を受け取るにはpropsを使う
+  // props: ['message'],
+  // propsにデフォルト値や型を指定する場合
+  props: {
+    message: {
+      type: String,
+      default: 'Like'
+    }
+  },
   // componentのdataは関数で返す
   data: function() {
     return {
       count: 0
     }
   },
-  template: '<button @click="countUp">Like {{ count }}</button>',
+  template: '<button @click="countUp">{{ message }} {{ count }}</button>',
   methods: {
     countUp: function() {
       this.count++;
